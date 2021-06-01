502b400ce279046d6c3d5d2ccbe7d5aaa2b93d7c
coderj001 <amirajubolchi001@gmail.com>
2021-06-01 22:09:18 +0530

    docker setup config

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -50,12 +50,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ChatServerPlayground.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if bool(strtobool(os.environ.get('postgres'))):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASS'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
